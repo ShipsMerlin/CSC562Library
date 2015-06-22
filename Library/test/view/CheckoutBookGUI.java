ca4b3e98116389415e4a277d8d7c04eb41c16c2d
bwiens <bwiens@gmail.com>
2015-06-22 18:01:11 -0400

    Adding CheckoutBookGui Test.

---
+++ Library/test/view/CheckoutBookGUI.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class CheckoutBookGUI {
+
+	private static JFrame jframe;
+	
+	public void createAndShowGUI() {
+		frame = new Frame("CheckoutSwing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Container getContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
