283322d31c0d46bea5a2abb5a9bcc7a5feb75899
Manal <Manal@157.160.78.182>
2015-06-23 22:08:55 +0800

    fix the warning in Commend add Member

---
+++ Library/src/model/CommandAddMember.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * @author Manal Ibrahim 
+ *
+ */
+public class CommandAddMember extends Command
+{
+/**
+ * ML is member List gonne and new Member name 
+ */
+
+
+@Override
+protected boolean execute() {
+	// TODO Auto-generated method stub
+	MemberList ML = new MemberList();
+	String memberName = null;
+	ML.addMember(memberName);
+	return true;
+}
+	
+
+}
