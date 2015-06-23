6815e449fc46e8d287ff7daddbcc3b0489f0c417
em1419 <em1419@mct164l12>
2015-06-23 08:50:15 -0400

    Fixed errors in RunnerTests. Fixed errors in CheckoutBookGUI test.

--- Library/src/view/CheckoutBookGUI.java
+++ Library/src/view/CheckoutBookGUI.java
@@ -1,23 +1,44 @@
 package view;
 
 import java.awt.Container;
+import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CheckoutBookGUI {
 
-	private static JFrame jframe;
-	
-	public void createAndShowGUI() {
-		jframe = new JFrame("CheckoutSwing");
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private static JFrame jFrame;
+
+	/**
+	 * Creates the components for the Checkout Book GUI
+	 */
+	public void createAndShowGUI()
+	{
+		jFrame = new JFrame("CheckoutSwing");
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JPanel content = new JPanel();
+		content.setLayout(new FlowLayout());
+		content.setName("myPanel");
+		final JTextField x = new JTextField(20);
+		x.setName("txtMemberSearch");
 		
+		content.add(x);
+		
+		jFrame.getContentPane().add(content);
+		jFrame.pack();
+		jFrame.setVisible(true);
 	}
 
-	public static Container getContainer() {
+	/**
+	 * @return - returns the JFrame object
+	 */
+	public static Container getContainer()
+	{
 		// TODO Auto-generated method stub
-		return null;
+		return jFrame;
 	}
 
 	
