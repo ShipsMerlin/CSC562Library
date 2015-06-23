f4a1138ad5b59294eea37f6643ad837559c64767
Manal <Manal@157.160.78.182>
2015-06-23 20:26:22 +0800

    Fixing the errors after adding the Warning Javadoc on Packege

--- Library/src/model/MemberList.java
+++ Library/src/model/MemberList.java
@@ -3,18 +3,32 @@ package model;
 import datasource.MemberRowDataGatewayMock;
 import datasource.DatabaseException;
 
-public class MemberList {
+/**
+ * @author Manal
+ *
+ */
+public class MemberList 
+{
 
 
 
 
-	public String addBook (String isbn, String title, String author) {
-		String memberName=null;
+	/**
+	 * @param memberName
+	 * @return memberRowDataGatewayMock.addMemberName
+	 */
+	public String addMember(String memberName) {
+		//String memberName=null;
 		MemberRowDataGatewayMock memberRowDataGatewayMock = new MemberRowDataGatewayMock ( memberName);
 		return memberRowDataGatewayMock.addMemberName();
 	}
-	public String getBook (String isbn) throws DatabaseException {
-		String memberName=null;
+	/**
+	 * @param memberName
+	 * @return memberRowDataGatewayMock.addMemberName
+	 * @throws DatabaseException
+	 */
+	public String getBook (String memberName) throws DatabaseException {
+		//String memberName=null;
 		MemberRowDataGatewayMock memberRowDataGatewayMock = new MemberRowDataGatewayMock( memberName);
 		return memberRowDataGatewayMock.addMemberName();
 
